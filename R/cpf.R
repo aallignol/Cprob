ad11b07e9e562f151ffaddb71bf8356df1c7c08d
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2010-02-10 12:07:53 +0100

    Modification of cpf() to handle the changes made in prodlim prodlim()$cuminc is now a list. returns something but not tested Got to look when more than 2 competing events

--- R/cpf.R
+++ R/cpf.R
@@ -20,25 +20,27 @@ cpf <- function(formula, data, subset, na.action, conf.int = 0.95, failcode) {
     
 ### Calcul de CP
     if (missing(failcode)) {
-        failcode <- ind <- as.numeric(colnames(fit$n.event)[1])
+        failcode <- ind <- as.numeric(names(fit$n.event)[1])
     }
     else {
-        if (!(failcode %in% colnames(fit$cuminc))) {
+        if (!(failcode %in% names(fit$cuminc))) {
             stop("'failcode' is not amongst the event types observed in the data")
         }
-        ind <- which(failcode == colnames(fit$cuminc))
+        ind <- which(failcode == names(fit$cuminc))
     }
-    if (ncol(fit$cuminc) > 2) {
-        cif.other <- rowSums(fit$cuminc[, -ind])
-        n.event.other <- rowSums(fit$n.event[, -ind])
+    if (length(fit$cuminc) > 2) {
+        cif.other <- do.call(fit$cuminc[-ind], "+")
+        n.event.other <- do.call(fit$n.event[-ind], "+")
+##         cif.other <- rowSums(fit$cuminc[, -ind])
+##         n.event.other <- rowSums(fit$n.event[, -ind])
     }
     else {
-        cif.other <- fit$cuminc[, -ind]
-        n.event.other <- fit$n.event[, -ind]
+        cif.other <- fit$cuminc[[-ind]]
+        n.event.other <- fit$n.event[[-ind]]
     }
-    n.event <- data.frame(fit$n.event[, ind], n.event.other)
-    names(n.event) <- c(as.name(failcode), "other")
-    cp <- fit$cuminc[, ind] / (1 - cif.other)
+    ## n.event <- data.frame(fit$n.event[, ind], n.event.other)
+    ## names(n.event) <- c(as.name(failcode), "other")
+    cp <- fit$cuminc[[ind]] / (1 - cif.other)
     
 ### Variance computation
     ## getting S(t-)
@@ -47,20 +49,22 @@ cpf <- function(formula, data, subset, na.action, conf.int = 0.95, failcode) {
     sminus[size[-length(size)]] <- 1
     if (length(fit$size.strata) == 1) {
         var.cp <- (sminus^2 / (1 - cif.other)^4) *
-            cumsum(((1 - cif.other)^2 * fit$n.event[, ind] + fit$cuminc[, ind]^2 * n.event.other) /
+            cumsum(((1 - cif.other)^2 * fit$n.event[[ind]] + fit$cuminc[[ind]]^2 * n.event.other) /
                    (fit$n.risk * (fit$n.risk - 1)))
     }
     else {
         ns <- c(0, cumsum(fit$size.strata))
         var.cp <- lapply(1:length(fit$size.strata), function(i) {
-            temp <- (sminus[(ns[i]+1):ns[i+1]]^2 / (1 - cif.other[(ns[i]+1):ns[i+1]])^4) *
-                cumsum(((1 - cif.other[(ns[i]+1):ns[i+1]])^2 * fit$n.event[(ns[i]+1):ns[i+1], ind] +
-                        fit$cuminc[(ns[i]+1):ns[i+1], ind]^2 * n.event.other[(ns[i]+1):ns[i+1]]) /
+            temp <- (sminus[(ns[i] + 1):ns[i + 1]]^2 / (1 - cif.other[(ns[i]+1):ns[i+1]])^4) *
+                cumsum(((1 - cif.other[(ns[i]+1):ns[i+1]])^2 * fit$n.event[[ind]][(ns[i]+1):ns[i+1]] +
+                        fit$cuminc[[ind]][(ns[i]+1):ns[i+1]]^2 * n.event.other[(ns[i]+1):ns[i+1]]) /
                        (fit$n.risk[(ns[i]+1):ns[i+1]] * (fit$n.risk[(ns[i]+1):ns[i+1]] - 1)))
             temp
         })
         var.cp <- do.call(c, var.cp)
     }
+
+    return(list(cp, var.cp))
     
     level <- 1 - conf.int
     upper <- cp + qnorm(1 - level/2) * sqrt(var.cp)
